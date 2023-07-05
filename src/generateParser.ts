1094186b20bae8132bc7a3e5402099f47b457f5e
Michal Grňo <m93a.cz@gmail.com>
2023-07-05 16:24:26 +0200

    add formatting to build step

--- src/generateParser.ts
+++ src/generateParser.ts
@@ -1,18 +1,21 @@
-import peggy from "npm:peggy";
+import peggy from 'npm:peggy';
 
-const IN_DIR = "src/grammar/";
-const OUT_DIR = "src/generated/";
+const IN_DIR = 'src/grammar/';
+const OUT_DIR = 'src/generated/';
 
 for await (const { name, isFile } of Deno.readDir(IN_DIR)) {
-  if (!isFile || !name.endsWith(".peggy")) continue;
+  if (!isFile || !name.endsWith('.peggy')) continue;
   const bareName = name.match(/^(.*)\.peggy$/)![1];
 
   const grammar = await Deno.readTextFile(IN_DIR + name);
-  const parser = peggy.generate(grammar, { output: "source", format: "es" });
+  const parser = peggy.generate(grammar, { output: 'source', format: 'es' });
   const output = `\
 // deno-lint-ignore-file
 
 ${parser}`;
 
-  await Deno.writeTextFile(OUT_DIR + bareName + ".js", output);
+  await Deno.writeTextFile(OUT_DIR + bareName + '.js', output);
 }
+
+const cmd = new Deno.Command('deno', { args: ['fmt', 'src/generated'] });
+await cmd.output();
